ab9f4756072fe882f121f9a5bbf424aaa2e57f13
kai0127 <83430662+kai0127@users.noreply.github.com>
2023-07-03 02:49:52 -0400

    Update prime_numbers.py

--- Math/prime_numbers.py
+++ Math/prime_numbers.py
@@ -1,1 +1,21 @@
-prime numbers
+# sieve of Eratosthenes  --->  O(nloglogn)
+primes = [False, False] + [True] * (n - 1)
+for i in range(2, n + 1):  # O(n)
+    if primes[i]:
+        for j in range(i * i, n + 1, i): # sum( n/p ) ~ O(loglogn)
+            primes[j] = False
+
+# sieve of Euler
+primes = []
+is_prime = [True] * (n+1)
+for i in range(2, n + 1):  # ~ O(n)
+    if is_prime[i]:
+        primes.append(i)
+        
+    for p in primes:
+        if p * i > n:
+            break
+        else:
+            is_prime[ p * i ] = False
+        if i % p == 0:
+            break
